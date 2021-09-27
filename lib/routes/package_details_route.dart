f0c2c3640af343214a3ade4707ab1be7138a9834
Kaleb Dias da Silva <kaleb.silva@ITB001106.itbam.org.br>
2021-09-27 07:41:30 -0400

    refactor: updata send params to other screen

--- lib/routes/package_details_route.dart
+++ lib/routes/package_details_route.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:nuvigator/next.dart';
+import 'package:proj/models/package_model.dart';
+import 'package:proj/models/producer_model.dart';
 import 'package:proj/screens/package_details_screen.dart';
 
-class PackageDetailsRoute extends NuRoute {
-  @override
-  Widget build(BuildContext context, NuRouteSettings<Object> settings) {
-    return PackageDetailsScreen(
-      package: settings.rawParameters['package'],
-      producer: settings.rawParameters['producer'],
+class PackageDetailsArgs {
+  PackageDetailsArgs({this.producer, this.package});
+
+  final Producer producer;
+  final Package package;
+
+  static PackageDetailsArgs fromJson(Map<String, dynamic> json) {
+    return PackageDetailsArgs(
+      producer: json['producer'],
+      package: json['package'],
     );
   }
+}
+
+class PackageDetailsRoute
+    extends NuRoute<NuRouter, PackageDetailsArgs, String> {
+  @override
+  Widget build(
+      BuildContext context, NuRouteSettings<PackageDetailsArgs> settings) {
+    //Map<String, dynamic> com a linha de arqumentos passados para a rota
+    print(settings.rawParameters);
+
+    // Instancia de PackageDetails com os argumentos parseados
+    print(settings.arguments);
+
+    print(settings.args);
+
+    return PackageDetailsScreen(
+      package: settings.args.package,
+      producer: settings.args.producer,
+    );
+  }
+
+  @override
+  ParamsParser<PackageDetailsArgs> get paramsParser =>
+      PackageDetailsArgs.fromJson;
 
   @override
   String get path => 'package-details';
